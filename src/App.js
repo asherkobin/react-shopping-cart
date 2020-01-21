8fa72f303761addcb456ec491d7fcd2254bbb9fe
Asher Kobin <Asher@Ashers-MBP.fios-router.home>
2020-01-20 19:34:38 -0500

    MVP

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
+import { ProductContext } from "./contexts/ProductContext";
+import { CartContext } from "./contexts/CartContext";
 
 // Components
 import Navigation from './components/Navigation';
@@ -12,29 +14,24 @@ function App() {
 	const [cart, setCart] = useState([]);
 
 	const addItem = item => {
-		// add the given item to the cart
+		setCart([...cart, item]);
 	};
 
 	return (
 		<div className="App">
-			<Navigation cart={cart} />
-
-			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
-			/>
-
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
+			<CartContext.Provider value={{ cart }}>
+				<Navigation />
+			</CartContext.Provider>
+			<ProductContext.Provider value={{ products, addItem }}>
+				<Route exact path="/">
+					<Products	/>
+				</Route>
+			</ProductContext.Provider>
+			<CartContext.Provider value={{ cart }}>
+				<Route path="/cart">
+					<ShoppingCart />
+				</Route>
+			</CartContext.Provider>
 		</div>
 	);
 }
